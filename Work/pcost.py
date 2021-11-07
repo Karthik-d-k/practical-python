6d447e4069a77536519a426eb44c7fa0ec9f50ca
karthikdk <karthikdk1998@gmail.com>
2021-11-07 12:43:01 +0530

    complete pcost.py

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,20 @@
 # pcost.py
 #
 # Exercise 1.27
+
+n_stocks = []
+stock_prices = []
+Total_cost = 0
+
+with open('Data/portfolio.csv', 'rt') as f :
+    headers = next(f)
+    for line in f:
+        n_stocks.append(line.split(',')[1])
+        stock_prices.append(line.split(',')[2].strip())
+     
+n_stocks = [int(n) for n in n_stocks]
+stock_prices =  [float(p) for p in stock_prices]
+
+Total_cost = sum([(n * p) for n, p in zip(n_stocks, stock_prices)])
+
+print(f"{Total_cost=}")
