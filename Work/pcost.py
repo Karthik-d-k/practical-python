ebeec1329f89997609bc0c19d6671d42c65aefa4
karthikdk <karthikdk1998@gmail.com>
2021-11-07 15:12:39 +0530

    handling missing data

--- Work/pcost.py
+++ Work/pcost.py
@@ -2,19 +2,25 @@
 #
 # Exercise 1.27
 
-n_stocks = []
-stock_prices = []
-Total_cost = 0
+def portfolio_cost(filename):
 
-with open('Data/portfolio.csv', 'rt') as f :
-    headers = next(f)
-    for line in f:
-        n_stocks.append(line.split(',')[1])
-        stock_prices.append(line.split(',')[2].strip())
-     
-n_stocks = [int(n) for n in n_stocks]
-stock_prices =  [float(p) for p in stock_prices]
+    n_stocks = []
+    stock_prices = []
+    Total_cost = 0
 
-Total_cost = sum([(n * p) for n, p in zip(n_stocks, stock_prices)])
+    with open(filename, 'rt') as f :
+        headers = next(f)
+        for line in f:
+            n_stocks.append(line.split(',')[1])
+            stock_prices.append(line.split(',')[2].strip())    
+    
+    n_stocks = [int(n) if n != '' else 0 for n in n_stocks]
+    stock_prices =  [float(p) if p != '' else 0  for p in stock_prices]
 
-print(f"{Total_cost=}")
+    Total_cost = sum([(n * p) for n, p in zip(n_stocks, stock_prices)])
+    print(f"{n_stocks=} {stock_prices=}")
+    
+    return Total_cost
+
+cost = portfolio_cost('Data/portfolio.csv')
+print('Total cost:', cost)
