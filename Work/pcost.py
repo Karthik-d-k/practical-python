de2579ca9e3c4a0bf709117b44a35820cc7ea0a4
karthikdk <karthikdk1998@gmail.com>
2021-11-07 15:27:33 +0530

    use csv builtin module

--- Work/pcost.py
+++ Work/pcost.py
@@ -2,8 +2,12 @@
 #
 # Exercise 1.27
 
-def portfolio_cost(filename):
+import csv
 
+def portfolio_cost(filename):
+    """ 
+    w/o using csv builtin module to find total portfolio cost
+    """
     n_stocks = []
     stock_prices = []
     Total_cost = 0
@@ -22,5 +26,28 @@ def portfolio_cost(filename):
     
     return Total_cost
 
-cost = portfolio_cost('Data/portfolio.csv')
+def portfolio_cost_using_csv(filename):
+    """ 
+    using csv builtin module to find total portfolio cost
+    """
+    n_stocks = []
+    stock_prices = []
+    Total_cost = 0
+
+    with open(filename) as f :
+        rows = csv.reader(f)
+        headers = next(rows)
+        for line in rows:
+            n_stocks.append(line[1])
+            stock_prices.append(line[2])    
+    
+    n_stocks = [int(n) if n != '' else 0 for n in n_stocks]
+    stock_prices =  [float(p) if p != '' else 0  for p in stock_prices]
+
+    Total_cost = sum([(n * p) for n, p in zip(n_stocks, stock_prices)])
+    print(f"{n_stocks=} {stock_prices=}")
+    
+    return Total_cost
+
+cost = portfolio_cost_using_csv('Data/portfolio.csv')
 print('Total cost:', cost)
