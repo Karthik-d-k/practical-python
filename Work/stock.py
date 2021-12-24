2936447014a34980c642970664854662b968e357
karthikdk <karthikdk1998@gmail.com>
2021-12-24 21:41:49 +0530

    use typed properties

--- Work/stock.py
+++ Work/stock.py
@@ -2,29 +2,25 @@
 #
 # Exercise 4.1
 
+from typedproperty import typedproperty
+from typedproperty import String, Integer, Float
+
 class Stock:
-    __slots__ = ('name','_shares','price')
+    name = String('name')
+    shares = Integer('shares')
+    price = Float('price')
+
     def __init__(self, name, shares, price):
         self.name = name
         self.shares = shares
         self.price = price
-    
+
     @property
     def cost(self):
         return (self.shares * self.price)
 
     def sell(self, nshares):
         self.shares -= nshares
-        
+
     def __repr__(self):
         return f'Stock({self.name},{self.shares},{self.price})'
-        
-    @property
-    def shares(self):
-        return self._shares
-
-    @shares.setter
-    def shares(self, value):
-        if not isinstance(value, int):
-            raise TypeError('Expected int')
-        self._shares = value
