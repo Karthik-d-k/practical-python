e5e9db90dbfb968476e164247dc3a8b8f77238ad
karthikdk <karthikdk1998@gmail.com>
2021-12-25 18:13:39 +0530

    create package

---
+++ Work/porty-app/setup.py
@@ -0,0 +1,12 @@
+# setup.py
+
+import setuptools
+
+setuptools.setup(
+    name="porty",
+    version="0.0.1",
+    author="Karthik D K",
+    author_email="karthikdk1998@gmail.com",
+    description="Practical Python Code",
+    packages=setuptools.find_packages(),
+)
